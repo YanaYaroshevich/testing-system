2ac62d4dd4289c047cf601913015d71730f4e08c
Yana Yaroshevich <yaroshevich.yana@gmail.com>
2015-11-30 13:50:23 +0300

    ui-grid questions on test-page

--- app/test-page/test-page.controller.js
+++ app/test-page/test-page.controller.js
@@ -34,6 +34,39 @@ angular.module('myApp.testPage')
         { name: 'passed', cellTemplate: '<div class="ngCellText" style="color: green; text-align: center" ng-if="row.entity.passed"><i class="fa fa-check"></i></div><div class="ngCellText" style="color: red; text-align: center" ng-if="!row.entity.passed"><i class="fa fa-times"></i></div>', width: '100', enableFiltering: false}
     ];
     
+    $scope.gridQuestions = {
+        enableFiltering: true
+    };
+    
+    $scope.toShowTypes = ['Text question and text answers', 'Text question and picture answers', 'Fill-the-word question', 'Text question with picture and text answers'];
+    
+    $scope.gridQuestions.data = testToShow.questions.map(function(quest){
+        var toReturn = {};
+        toReturn.rightAnswers = '';
+        if (quest.type === 0) {
+            for (var i = 0; i < quest.answers.length; i++) {
+                if (quest.answers[i].right){
+                    toReturn.rightAnswers += quest.answers[i].text + '; ';
+                }
+            }
+            toReturn.text = quest.text;
+        }
+        else if (quest.type === 2) {
+            toReturn.rightAnswers = quest.text.substring(quest.text.indexOf('###'), quest.text.lastIndexOf('###'));
+            toReturn.text = quest.text.replace(toReturn.rightAnswers, '???').replace('###', '');
+        }
+        toReturn.type = $scope.toShowTypes[quest.type];
+        toReturn.cost = quest.cost;
+        return toReturn;
+    });
+    
+    $scope.gridQuestions.columnDefs = [
+        { name: 'text', headerCellClass: 'header-filtered', width: '150' },
+        { name: 'cost', headerCellClass: 'header-filtered', width: '80' },
+        { name: 'type', headerCellClass: 'header-filtered', width: '150' },
+        { name: 'rightAnswers', enableFiltering: false,  width: '150' }
+    ];
+    
     console.log($scope.test);
     
     $rootScope.showLeftMenu = true;
