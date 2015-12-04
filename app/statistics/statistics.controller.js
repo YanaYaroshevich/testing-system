7c238b3612c60b79783828123fda852e420628ce
Yaroshevich Yana <yyaroshevich@exadel.by>
2015-12-04 10:03:13 +0300

    PieCharts for passed/not passed students

--- app/statistics/statistics.controller.js
+++ app/statistics/statistics.controller.js
@@ -2,59 +2,79 @@
 
 angular.module('myApp.statistics')
 
-.controller('StatisticsPageCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+.controller('StatisticsPageCtrl', ['$scope', '$rootScope', 'ngNotify', '$http', '$q', function($scope, $rootScope, ngNotify, $http, $q) {
     $scope.pageName = 'Statistics';
     $rootScope.showLeftMenu = true;
     
+    $scope.testsStatistics = [];
+    var testRequests = [];
+    $scope.pieChartsData = [];
+    var studentsPassed = 0;
+    var studentsNotPassed = 0;
+    
+    $http.get('/tests/page/' + $rootScope.id).then(function(res){
+        for (var i = 0; i < res.data.tests.length; i++){
+            testRequests.push($http.get('/test/page/' + res.data.tests[i]._id));
+        }
+        $q.all(testRequests).then(function (results) {
+           for (var i = 0; i < results.length; i++) {
+               studentsPassed = 0;
+               studentsNotPassed = 0;
+               for (var j = 0; j < results[i].data.test.students.length; j++){
+                   if (results[i].data.test.students[j].passed){
+                       studentsPassed++;
+                   }
+                   else {
+                       studentsNotPassed++;
+                   }
+               }
+               $scope.testsStatistics.push({passed: studentsPassed, notPassed: studentsNotPassed, name: results[i].data.test.name});
+               
+           }
+            for (var i = 0; i < $scope.testsStatistics.length; i++){
+               $scope.pieChartsData.push([{key: "Passed", y: $scope.testsStatistics[i].passed}, {key: "Not passed", y: $scope.testsStatistics[i].notPassed}]);
+            }
+            console.log($scope.pieChartsData);
+        }, function (err) {
+            ngNotify.set(err.statusText);
+        });
+    }, function(err){
+        ngNotify.set(err.data);
+    });
+    
     $scope.options = {
-            chart: {
-                type: 'pieChart',
-                height: 500,
-                x: function(d){return d.key;},
-                y: function(d){return d.y;},
-                showLabels: false,
-                duration: 500,
-                legend: {
-                    margin: {
-                        top: 5,
-                        right: 35,
-                        bottom: 5,
-                        left: 0
-                    }
+        chart: {
+            type: 'pieChart',
+            height: 200,
+            x: function(d){return d.key;},
+            y: function(d){return d.y;},
+            showLabels: false,
+            duration: 500,
+            legend: {
+                margin: {
+                    top: 5,
+                    right: 35,
+                    bottom: 5,
+                    left: 0
                 }
             }
-        };
+        }
+    };
     
-   $scope.data = [
+
+    
+    
+    
+  /* $scope.data = [
             {
-                key: "One",
-                y: 5
+                key: "Passed",
+                y: studentsPassed
             },
             {
-                key: "Two",
-                y: 2
-            },
-            {
-                key: "Three",
-                y: 9
-            },
-            {
-                key: "Four",
-                y: 7
-            },
-            {
-                key: "Five",
-                y: 4
-            },
-            {
-                key: "Six",
-                y: 3
-            },
-            {
-                key: "Seven",
-                y: .5
+                key: "Not passed",
+                y: studentsNotPassed
             }
-        ];
+        ];*/
     
     /*--------------------------------------------*/
     
