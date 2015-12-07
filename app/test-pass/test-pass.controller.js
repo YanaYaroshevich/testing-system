2bca84d0701363889f1b74aeeb90d655b0cd0463
Yaroshevich Yana <yyaroshevich@exadel.by>
2015-12-07 22:51:53 +0300

    pretty-checkbox, question text refactor

--- app/test-pass/test-pass.controller.js
+++ app/test-pass/test-pass.controller.js
@@ -9,12 +9,12 @@ angular.module('myApp.testPage')
     
     $scope.questions = test.questions;
     
+    
+    
     $scope.currentPage = 1;
     $scope.totalItems = $scope.questions.length;
     $scope.itemsPerPage = 1;
     $scope.maxSize = 3;
-    
-    
     
     $scope.pageChanged = function() {
         
